b06836d5610c5fed8391e6ffe0691f01a42f727f
leminhthien2011 <leminhthien2011@gmail.com>
2020-10-29 10:14:51 -0400

    Update summaryConet.R

--- R/summaryConet.R
+++ R/summaryConet.R
@@ -1,10 +1,23 @@
 #' This gives descriptive stat
 #' @param x numeric vector
 #' @example
+#' x = 1:9
+#' summaryConet(x)
+#' A tibble: 1 x 9
+#' len  mean    sd   min   max   med firstpt thirdpt   IQR
+#' <int> <dbl> <dbl> <int> <int> <int>   <dbl>   <dbl> <dbl>
+#'  1     9     5  2.74     1     9     5       3       7     4
 #' summaryConet(x)
 #' @export
 summaryConet <- function(x){
-  tibble(min = min(x),
-             max = max(x),
-             med = median(x))
+  tibble(len = length(x),
+         mean = mean(x),
+         sd = sd(x),
+         min = min(x),
+         max = max(x),
+         med = median(x),
+         firstpt = quantile(x,.25),
+         thirdpt = quantile(x, .75),
+         IQR = thirdpt - firstpt)
+
 }
