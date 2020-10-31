387ebac05b1276279def6ac51440f96fbc73db18
leminhthien2011 <leminhthien2011@gmail.com>
2020-10-30 21:00:26 -0400

    This is 10-30 version of CONETTravel

--- R/summaryConet.R
+++ R/summaryConet.R
@@ -1,12 +1,15 @@
 #' This gives descriptive stat
 #' @param x numeric vector
-#' @example
+#' @import tibble
+#' @importFrom stats median quantile sd
+#' @examples
 #' x = 1:9
 #' summaryConet(x)
-#' A tibble: 1 x 9
+#' \dontrun{A tibble: 1 x 9
 #' len  mean    sd   min   max   med firstpt thirdpt   IQR
 #' <int> <dbl> <dbl> <int> <int> <int>   <dbl>   <dbl> <dbl>
-#'  1     9     5  2.74     1     9     5       3       7     4
+#'  9     5  2.74     1     9     5       3       7     4
+#'  }
 #' summaryConet(x)
 #' @export
 summaryConet <- function(x){
