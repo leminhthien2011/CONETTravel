ac6051458f88f69fefbb2bc9327893be7f307b72
leminhthien2011 <leminhthien2011@gmail.com>
2020-11-03 16:37:41 -0500

    Nov 3 4h37

---
+++ R/datas_3travel-data.R
@@ -0,0 +1,9 @@
+#' datas_3travel here
+#' Description of your data
+#' @docType data
+#' @usage data(datas_3travel)
+#' @format An object of class blablabla
+#' @keywords datasets
+#' @examples
+#' data(datas_3travel)
+"datas_3travel"
