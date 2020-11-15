419d6c30bccc95d9cd871023828e2a769c65e64a
leminhthien2011 <leminhthien2011@gmail.com>
2020-11-15 10:59:00 -0500

    nov15

--- R/populationdynamic_func.R
+++ R/populationdynamic_func.R
@@ -1,14 +1,14 @@
 #' This gives estimation for the dynamic population
-#' @param country list include total travel in and out each day total_in,
-#' total_out, duration nrep, population P
+#' @param country list include total travel in: total_in, total travel out each day:;
+#' total_out, durationtravel, population P
 #' @export
 
-popfunc = function(country){
+populationdynamicfunc = function(country){
   fluc1 = country$total_in - country$total_out
-  pop = rep(0,country$nrep)
+  pop = rep(0,country$durationtravel)
   pop[1] = country$P
 
-  for(i in 2:country$nrep){
+  for(i in 2:country$durationtravel){
     x = pop[i-1]
     pop[i] = x+ fluc1[i] #update total population
   }
