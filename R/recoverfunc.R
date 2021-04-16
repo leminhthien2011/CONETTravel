58a60ef01e458ed0a6b75261b89fc7e6d6ec7150
leminhthien2011 <leminhthien2011@gmail.com>
2021-04-16 14:51:55 -0400

    update

--- R/recoverfunc.R
+++ R/recoverfunc.R
@@ -3,6 +3,29 @@
 #' @param country is a list include A (active confirmed), R(Recovered confirmed),
 #'  D(Confirmed Deceased), populationdynamic,and infect
 #' @param theta parameter
+#' @examples
+#' \dontrun{
+#' library(CONETTravel)
+#' library(rlist)
+#' data = datas_3travel[[1]][,3:5]
+#' betas = betafunc(data)
+#' x = c(9999620, 250,130,0,0,0)
+#' P= sum(x)
+#' durationtravel = 84
+#' w21= travelout_3dat[1,1]/(travelout_3dat[1,1] + travelout_3dat[1,3])
+#' w31= travelout_3dat[1,1]/(travelout_3dat[1,1] + travelout_3dat[1,2])
+#' out21 = travelout_3dat[,2]
+#' out31 = travelout_3dat[,3]
+#' total_in = out21*w21 + out31*w31
+#' total_out = travelout_3dat[,1]
+#' country = list(P =P, total_in = total_in, total_out= total_out,durationtravel=durationtravel)
+#' populationdynamic = populationdynamicfunc(country)
+#' infect = c(infectfunc(data, x),0)
+#' country =list.append(country, data=data, populationdynamic=populationdynamic,
+#'                     infect= infect)
+#'                     theta = thetas_3travel[[1]][1:6]
+#'                     recoverfunc(country,theta)
+#' }
 #' @export
 recoverfunc = function(country, theta){
 
